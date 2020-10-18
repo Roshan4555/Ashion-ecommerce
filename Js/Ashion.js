705ea4e4b60a0f3d1e62312f4f5a2a2d62ac600b
Roshan <surukutlaroshan@gmail.com>
2020-10-18 20:50:28 +0530

    All product changes

--- Js/Ashion.js
+++ Js/Ashion.js
@@ -2,6 +2,10 @@ var hidediv = document.getElementById("hiding-div");
 var opendiv = document.getElementById("navbar-outer-box");
 var closediv = document.getElementById("grid-container");
 var close = document.getElementById('close-button');
+var navbar = document.getElementById('large-screen-navbar');
+var navbar_links = document.getElementById('links-display').innerHTML;
+var cart_emojis = document.getElementById('cart-div').innerHTML;
+
 opendiv.addEventListener('click',()=>{ 
     hidediv.style.display='block';
 })
@@ -15,6 +19,15 @@ $('.navbar-outer-box').click(function() {
   $('#close-button, #grid-container').click(function(e){    
     $('.hiding-div').fadeOut('slow');
   });
+  // navbar.innerHTML = cart_emojis;
+// navbar.innerHTML.style.display = 'block';
+navbar.innerHTML =  navbar_links;
+// navbar_links.style.display = 'inline-block';
+navbar.classList.add('links-display-for-large-screen');
+// navbar.classList.add('cart-liked-emojis');
+
+
+  // Javascript timer display
   var Count_down_date = new Date('Jan 5, 2021 15:50:25').getTime();
   var x = setInterval(function(){
     var now = new Date().getTime();
